9d55f43097826673b61fcb2bd344a0b35433f99f
GitHub Action <action@github.com>
2022-05-24 20:26:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/galiagacandia/hibernatequeryconsole/MyBundle.kt
+++ src/main/kotlin/com/github/galiagacandia/hibernatequeryconsole/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.galiagacandia.hibernatequeryconsole
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/galiagacandia/hibernatequeryconsole/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.galiagacandia.hibernatequeryconsole.services
+
+import com.github.galiagacandia.hibernatequeryconsole.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
